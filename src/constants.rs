c3e1366e3a3dec720a5e862a0a16fa9dfae08cd5
asjn3e <asjn3e@gmail.com>
2024-10-21 17:07:35 +0400

    deploy an instance of evm_rpc canister for appic use only on a subnet with 13 nodes

--- src/constants.rs
+++ src/constants.rs
@@ -27,9 +27,9 @@ pub const RPC_SERVICE_MAX_SIZE: u32 = 256;
 pub const AUTH_SET_STORABLE_MAX_SIZE: u32 = 1000;
 pub const WASM_PAGE_SIZE: u64 = 65536;
 
-pub const ETH_GET_LOGS_MAX_BLOCKS: u32 = 500;
+pub const ETH_GET_LOGS_MAX_BLOCKS: u32 = 1000;
 
-pub const NODES_IN_SUBNET: u32 = 34;
+pub const NODES_IN_SUBNET: u32 = 13;
 
 pub const API_KEY_REPLACE_STRING: &str = "{API_KEY}";
 pub const VALID_API_KEY_CHARS: &str =
